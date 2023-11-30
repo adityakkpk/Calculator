a83e3e54518ce81e1418c0a7663d790700a0a28d
Aditya <akkpk933@gmail.com>
2023-11-30 15:13:52 +0530

    made responsive

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let calculation = localStorage.getItem('calculation') || '';
 
-        // Display the calculation when the page first loads.
-        displayCalculation();
+// Display the calculation when the page first loads.
+displayCalculation();
 
-        function updateCalculation(value) {
-            calculation += value;
-            
-            // Display the calculation on the page
-            // instead of console.log
-            displayCalculation();
+function updateCalculation(value) {
+    calculation += value;
+    
+    // Display the calculation on the page
+    // instead of console.log
+    displayCalculation();
 
-            localStorage.setItem('calculation', calculation);
-        }
+    localStorage.setItem('calculation', calculation);
+}
 
-        function displayCalculation() {
-            document.querySelector('.js-calculation')
-            .innerHTML = calculation;
-        }
+function displayCalculation() {
+    document.querySelector('.js-calculation')
+    .innerHTML = calculation;
+}
